65554f7c6b44a5ba1762f680dc7e3e7059d05743
Mark J Perryman <tazmancoder@gmail.com>
2023-03-30 07:34:32 -0600

    Video 5
    Add onboarding view controller

---
+++ Sources/FavOnboardingKit/Controller/OnboardingVC.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingVC.swift
+//  
+//
+//  Created by Mark Perryman on 3/30/23.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
--- Sources/FavOnboardingKit/FavOnboardingKit.swift
+++ Sources/FavOnboardingKit/FavOnboardingKit.swift
@@ -2,12 +2,19 @@ import UIKit
 
 public class FavOnboardingKit {
     
+    private var onboardingVC: OnboardingVC = {
+        let controller = OnboardingVC()
+        controller.modalTransitionStyle = .crossDissolve
+        controller.modalPresentationStyle = .fullScreen
+        return controller
+    }()
+    
     public init() {
         
     }
     
     public func launchOnboarding(rootVC: UIViewController) {
-        
+        rootVC.present(onboardingVC, animated: true, completion: nil)
     }
     
     public func dismissOnboarding() {
